b16bd53e4460ecc34cf5a2ca8104bb2fa55d62fb
Bahoque <bahoque.sena@gmail.com>
2025-10-16 15:58:38 -0500

    curso git3 actualizado

--- curso_git3.java
+++ curso_git3.java
@@ -1,9 +1,5 @@
 public class curso_git3 {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("hello git 3 v login!");
-=======
         System.out.println("hello git 3 v3!");
->>>>>>> main
     }
 }
